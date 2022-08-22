f77b0b851b9fcd294ee77af4698631bb8342343b
Tamirlan4 <tamirlan_agubaev@mail.ru>
2022-08-22 14:55:14 +0600

    Display

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Start...");
+        Display.create(800,600,"Tanks");
     }
 }
